a352cf8f4d3c8ed699229f6a71f257d3de60d767
a13con <a13kon@yandex.ru>
2023-10-17 16:51:24 +0300

    test swap C22 visibility by pressing create btn

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 "use strict";
 
+log();
+log();
+
+function log() {
+    console.log('click');
+
+        var obj = document.getElementById("divC01");
+        if (obj.style.display !="flex") {
+            obj.style.display = "flex";
+        }
+        else obj.style.display = "none";
+    }
+
 
 var createBtn = document.getElementById("createBtn");
-createBtn.addEventListener('click', () => console.log('click'));
+createBtn.addEventListener('click', log);
 
