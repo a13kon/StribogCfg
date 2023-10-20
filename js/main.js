b0b36790a39c26cf991b9e95ef891b9503e8aec4
a13con <a13kon@yandex.ru>
2023-10-20 12:51:51 +0300

    added listeners for hide options

--- js/main.js
+++ js/main.js
@@ -1,88 +1,125 @@
 "use strict";
 
-//получение всех div-ов для дальнейшего скрытия
-var divC01 = document.getElementById("divC01");
-var divC02 = document.getElementById("divC02");
-var divC03 = document.getElementById("divC03");
-var divC04 = document.getElementById("divC04");
-var divC05 = document.getElementById("divC05");
-var divC06 = document.getElementById("divC06");
-var divC07 = document.getElementById("divC07");
-var divC08 = document.getElementById("divC08");
-var divC09 = document.getElementById("divC09");
-var divC10 = document.getElementById("divC10");
- 
-//prepare скрытие при загрузке страницы
-setVisibility();
-
 //массив для формирования файла-конфигурации
-var configArr = [];
+let configArr = [];
 
 //слуаштели событий
-var createBtn = document.getElementById("createBtn");
-createBtn.addEventListener('click', () => createArrCfg("===========click===========", configArr));
+let createBtn = document.getElementById("createBtn");
+createBtn.addEventListener('click', () => {rows.getValue(); createArrCfg("===========click===========", rows.value, rows.quaentity)});
 
-document.getElementById("inpC03").addEventListener('click', setVisibility);
-document.getElementById("inpC04").addEventListener('click', setVisibility);
-document.getElementById("inpC08").addEventListener('click', setVisibility);
+document.getElementById("inpC03").addEventListener('click', () => {rows.getValue(); rows.setVisibility()});
 
-// var obj = document.getElementById("divC01");
-// if (obj.style.display !="flex") {
-//     obj.style.display = "flex";
-// }
-// else obj.style.display = "none";
+document.getElementById("inpC04").addEventListener('click', () => {
+    rows.getValue(); 
+    if (rows.value[3] == 0) {
+        document.getElementById("optC08par0").style.display = "contents";
+    }
+    else
+    {
+        document.getElementById("optC08par0").style.display = "none";
+        rows.state[7].value = 1;
+    }
+    rows.getValue(); 
+    rows.setVisibility();
+});
 
-//функция динамического отображения элементов и динамических значений
-function setVisibility() {
+document.getElementById("inpC05").addEventListener('click', function() {
+    rows.getValue();
+    if (rows.value[4] == 0) {
+        document.getElementById("optC06par4").style.display = "contents";
+        document.getElementById("optC06par5").style.display = "contents";
+        document.getElementById("optC06par6").style.display = "contents";
+        document.getElementById("optC06par7").style.display = "contents";
+        document.getElementById("optC06par8").style.display = "contents";
+         }
+    else {
+        document.getElementById("optC06par4").style.display = "none";
+        document.getElementById("optC06par5").style.display = "none";
+        document.getElementById("optC06par6").style.display = "none";
+        document.getElementById("optC06par7").style.display = "none";
+        document.getElementById("optC06par8").style.display = "none";
+        if (rows.value[5] > 3) {
+        rows.state[5].value = 3;
+        }
+    }
+});
 
-    if (getElemStatus("inpC03") > 0) {
-        divC04.style.display = "flex";
-        if (getElemStatus("inpC04") == 1){
-            divC05.style.display = "flex";
-            divC06.style.display = "flex";
+document.getElementById("inpC08").addEventListener('click', () => {rows.getValue(); rows.setVisibility()});
+
+document.getElementById("inpC14").addEventListener('click', () => {rows.getValue(); rows.setVisibility()});
+
+
+
+
+//получение div-ов для дальнейшего скрытия
+let rows = {
+    style : [], //стиль/видимость объекта 
+    value : [], //конечное значение int
+    state : [], //состояние элемента
+    quaentity : 22, //нужно чтобы рассчитывалось
+
+    getStyle() {
+        for (let i = 1; i <= this.quaentity; i++) {
+            rows.style[i - 1] = document.getElementById(i < 10 ? "divC0" + String(i) : "divC" + String(i));
+        }
+    },
+
+    getValue() {
+        for (let i = 1; i <= this.quaentity; i++) {
+            rows.value[i - 1] = this.getElemStatus(i < 10 ? "inpC0" + String(i) : "inpC" + String(i));
+        }
+    },
+
+    getState() {
+    for (let i = 1; i <= this.quaentity; i++) {
+        rows.state[i - 1] = document.getElementById(i < 10 ? "inpC0" + String(i) : "inpC" + String(i));
+        }
+    },    
+
+
+    //функция получения значения input
+    getElemStatus(elemId) {
+        var elem = document.getElementById(elemId);
+    
+        if (elem.type == "select-one") {
+        return elem.value ;
+        }
+        else if (elem.type == "checkbox") {
+            return elem.checked? 1 : 0;
         }
         else {
-            divC05.style.display = "none";
-            divC06.style.display = "none";
+            return "wrong type";
         }
-    }
-    else {
-        divC04.style.display = "none";
-        divC05.style.display = "none";
-        divC06.style.display = "none";
-    }
+    },
 
-    // getElemStatus("inpC03") > 0?                                    divC04.style.display = "flex" : divC04.style.display = "none";
-    // getElemStatus("inpC03") > 0 && getElemStatus("inpC04") == 1?    divC05.style.display = "flex" : divC05.style.display = "none";
-    // getElemStatus("inpC03") > 0 && getElemStatus("inpC04") == 1?    divC06.style.display = "flex" : divC06.style.display = "none";
-    getElemStatus("inpC03") > 1?                                    divC07.style.display = "flex" : divC07.style.display = "none";
-    getElemStatus("inpC03") > 1?                                    divC08.style.display = "flex" : divC08.style.display = "none";
-    getElemStatus("inpC03") > 1 && getElemStatus("inpC08") == 1?    divC09.style.display = "flex" : divC09.style.display = "none";
-    getElemStatus("inpC03") > 1 && getElemStatus("inpC08") == 1?    divC10.style.display = "flex" : divC10.style.display = "none";
+    //функция динамического отображения элементов и динамических значений
+     setVisibility() {
 
+        rows.getValue();                                       
+        rows.value[2] > 0?                            rows.style[3].style.display = "flex" : rows.style[3].style.display = "none";
+        rows.value[2] > 0 && rows.value[3] == 1?      rows.style[4].style.display = "flex" : rows.style[4].style.display = "none"; 
+        rows.value[2] > 0 && rows.value[3] == 1?      rows.style[5].style.display = "flex" : rows.style[5].style.display = "none"; 
+        rows.value[2] > 1?                            rows.style[6].style.display = "flex" : rows.style[6].style.display = "none"; 
+        rows.value[2] > 1?                            rows.style[7].style.display = "flex" : rows.style[7].style.display = "none"; 
+        rows.value[2] > 1 && rows.value[7] == 1?      rows.style[8].style.display = "flex" : rows.style[8].style.display = "none"; 
+        rows.value[2] > 1 && rows.value[7] == 1?      rows.style[9].style.display = "flex" : rows.style[9].style.display = "none"; 
+        rows.value[13] == 1?                          rows.style[14].style.display = "flex" : rows.style[14].style.display = "none"; 
+    
+    },
+    
 }
+ 
+//prepare скрытие при загрузке страницы
+rows.getStyle();
+rows.getState();
+rows.getValue();
+rows.setVisibility();
 
-//функция получения значения input
-function getElemStatus(elemId) {
-    var elem = document.getElementById(elemId);
 
-    if (elem.type == "select-one") {
-    return elem.value ;
-    }
-    else if (elem.type == "checkbox") {
-        return elem.checked;
-    }
-    else {
-        return "wrong type";
-    }
-}
-
-//заполнение массива
-function createArrCfg(msg, arr) {
-    console.log(msg); 
-      
-    for (let i = 1; i <= 22; i++) {
-         arr[i-1] = getElemStatus(i < 10 ? "inpC0" + String(i) : "inpC" + String(i));
+//в будущем создание файла, сейчас просто чек массива
+function createArrCfg(msg, arr, qua) {
+    console.log(msg);
+      for (let i = 1; i <= qua; i++) {
          console.log(`[${i-1}] = ${arr[i-1]}`);
     }
 }
