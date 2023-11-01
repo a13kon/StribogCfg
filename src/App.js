74dee002ed0332ba0d77b812cf4459215f5bd3dd
a13con <a13kon@yandex.ru>
2023-11-01 16:35:03 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React, {useState} from 'react';
+import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
 
 function App() {
-  const [likes, setLikes]  = useState(0);
   const [value, setValue] = useState('Text');
 
-  function increment() {
-    setLikes(likes + 1);
-  }
-
-  function decrement() {
-    setLikes(likes - 1);
-  }
 
   return (
     <div>
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input type="text" value={value}/>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <ClassCounter/>
     </div>
   );
 }
