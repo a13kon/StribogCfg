43dfa8a60af90300527fd52f1babff1afd181bd4
a13con <a13kon@yandex.ru>
2024-01-09 15:21:33 +0300

    added mongo to src/index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const error404 = require('../middleware/err-404');
+const indexRouter = require('../routers/index');
+const configsRouter = require('../routers/configs');
+
 app = express();
     
 app.use(express.json());
 app.use(express.urlencoded());
 app.set('view engine', 'ejs');
 
+app.use('/', indexRouter);
+app.use('/configs', configsRouter);
+
 app.use(error404);
 
+async function start(PORT, UrlDB) {
+    try {
+        await mongoose.connect(UrlDB, {
+            dbname: 'configs-storage'
+        });
+        app.listen(PORT, () => {
+            console.log(`STRIBOG-CFG is listening on port ${PORT}`)
+        });
+    } catch(e) {
+        console.log(e);
+    }
+}
 
-
-app.listen(3000, () => {
-    console.log('server listening');
-});
+const UrlDB = process.env.UrlDB;
+const PORT = process.env.PORT || 3000;
+start(PORT, UrlDB);
