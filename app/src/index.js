d8f0cc2ee54ab5521a2bfff8ad25d1db46a401af
a13kon <a13kon@yandex.ru>
2024-01-10 14:38:08 +0300

    fixed mongoose  authorization

--- app/src/index.js
+++ app/src/index.js
@@ -3,23 +3,23 @@ const mongoose = require('mongoose');
 
 const error404 = require('../middleware/err-404');
 const indexRouter = require('../routers/index');
-const configsRouter = require('../routers/configs');
+//const configsRouter = require('../routers/configs');
 
 app = express();
-    
+     
 app.use(express.json());
 app.use(express.urlencoded());
 app.set('view engine', 'ejs');
 
 app.use('/', indexRouter);
-app.use('/configs', configsRouter);
+//app.use('/configs', configsRouter);
 
 app.use(error404);
 
 async function start(PORT, UrlDB) {
     try {
         await mongoose.connect(UrlDB, {
-            dbname: 'configs-storage'
+            dbName: 'configs-storage'
         });
         app.listen(PORT, () => {
             console.log(`STRIBOG-CFG is listening on port ${PORT}`)
